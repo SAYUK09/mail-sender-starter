4f4cffeef45bac4fc7e13896910b88abbf6d69c3
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-09-13 22:44:46 +0530

    taking email address as input and send mails

--- pages/api/sendmails.ts
+++ pages/api/sendmails.ts
@@ -14,20 +14,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
-  console.log("enterd");
-  const msg: any = {
-    to: "sayurikamble962@gmail.com",
-    subject: "Testing.....",
-    from: {
-      email: "sayurikamble123@gmail.com",
-      name: "Sayuri",
-    },
-    text: "Still Testing...",
-  };
-  const data = await sgMail.send(msg);
-  if (data[0].statusCode === 202) {
-    res.status(200).json({ msg: "Mail Sent" });
-  } else {
-    console.log("errors");
+  if (req.method === "POST") {
+    console.log(req.body.emailAddress);
+    const msg: any = {
+      to: req.body.emailAddress,
+      subject: "Testing.....",
+      from: {
+        email: "sayurikamble123@gmail.com",
+        name: "Sayuri",
+      },
+      text: "Still Testing...",
+    };
+    const data = await sgMail.send(msg);
+    if (data[0].statusCode === 202) {
+      res.status(200).json({ msg: "Mail Sent" });
+    } else {
+      console.log("errors");
+    }
   }
 }
